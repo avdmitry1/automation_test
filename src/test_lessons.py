071c1574dd20e1475825b26c0fd651190cc7d573
avdmitry1 <avdmitry1@gmail.com>
2025-04-22 20:16:11 +0300

    updates

--- src/test_lessons.py
+++ src/test_lessons.py
@@ -1,9 +1,15 @@
 import pytest
 
 
+
 @pytest.fixture()
 def set_up():
-    print("Set up")
+    print("Intro test")
+    # yield is used to define generators. Here, it marks the end of
+    # the setup code and the start of the teardown code. The teardown
+    # code is run after the test has been run.
+    yield
+    print(" Outro test")
 
 
 def test_sending_mail_1(set_up):
