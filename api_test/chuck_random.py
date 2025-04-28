36e157e4cc3a93940ca2d4cbe28214d881608bde
avdmitry1 <avdmitry1@gmail.com>
2025-04-28 18:13:08 +0300

    new file

--- api_test/chuck_random.py
+++ api_test/chuck_random.py
@@ -2,18 +2,24 @@ import requests
 import unittest
 
 
-class TestNewJoke(unittest.TestCase):
-    def setUp(self):
-        self.url = "https://api.chucknorris.io/jokes/random"
+class TestRandomJoke(unittest.TestCase):
+    URL = "https://api.chucknorris.io/jokes/random"
+    URL_BY_CATEGORY = "https://api.chucknorris.io/jokes/random?category="
 
-    def test_create_new_random_joke(self):
-        response = requests.get(self.url)
+    def test_random_joke_creation(self):
+        response = requests.get(self.URL)
+        joke = response.json()
         self.assertEqual(response.status_code, 200, "Failed to get joke")
-        self.assertIsNotNone(response.json()["value"], "Joke value is null")
-        self.assertIsNotNone(response.json()["created_at"], "Joke created date is null")
-        assert isinstance(response.json()["created_at"], str)
-        assert isinstance(response.json()["categories"], list)
-        print("Test passed")
+        self.assertIsNotNone(joke["value"], "Joke value is null")
+        self.assertIsNotNone(joke["created_at"], "Joke created date is null")
+        self.assertIsInstance(joke["created_at"], str)
+        self.assertIsInstance(joke["categories"], list)
+
+    def test_random_joke_creation_by_category(self):
+        category = "sport"
+        response = requests.get(self.URL_BY_CATEGORY + category)
+        self.assertEqual(response.status_code, 200, "Failed to get joke")
+        self.assertEqual(response.json()["categories"][0], category)
 
 
 if __name__ == "__main__":
